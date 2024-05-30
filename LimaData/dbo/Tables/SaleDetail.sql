973ea9383a10d75528ed7b4696de9cf5af6229ec
Maxwell Aruakpor <maxgudly@gmail.com>
2024-05-30 13:36:53 -0400

    Created Tables for our db schema and published them

---
+++ LimaData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductId] INT NOT NULL, 
+    [SaleId] INT NOT NULL, 
+    [Quantiy] INT NOT NULL,
+    [PurchasePrice] MONEY NOT NULL DEFAULT 0, 
+    [Tax] MONEY NOT NULL
+)
